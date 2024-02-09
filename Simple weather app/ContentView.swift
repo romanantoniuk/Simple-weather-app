7fe0e45b9336b33fcfb0b93bee0854a2d84a89ea
Roman Antoniuk <roman.antoniuk@icloud.com>
2024-02-09 11:31:09 +0200

    Initial Commit

---
+++ Simple weather app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Simple weather app
+//
+//  Created by Roman Antoniuk on 09.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Simple weather app/Simple_weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_weather_appApp.swift
+//  Simple weather app
+//
+//  Created by Roman Antoniuk on 09.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Simple_weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
